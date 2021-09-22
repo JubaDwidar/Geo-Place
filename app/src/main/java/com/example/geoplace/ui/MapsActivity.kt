ab0d1969b883213e50d0ba46fd0b583a5f910b60
JubaDwidar <hdwedar4@gmail.com>
2021-09-22 15:26:06 +0200

    new

--- app/src/main/java/com/example/geoplace/ui/MapsActivity.kt
+++ app/src/main/java/com/example/geoplace/ui/MapsActivity.kt
@@ -69,7 +69,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
         mapView = mapFragment.requireView()
 
-        Places.initialize(this, "AIzaSyAwsEWZavp2cjDnC6u-JHdLy8EayA8TYlg")
+        Places.initialize(this, getString(string.google_maps_key));
         placesClient = Places.createClient(this)
         val token = AutocompleteSessionToken.newInstance()
 
